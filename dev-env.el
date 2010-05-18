25365bff4a1d7f3654ad0d72a740516414c4ac81
Wei LIU <liuw@liuw.name>
2010-05-18 21:16:51 +0800

    Set C coding style to linux

--- dev-env.el
+++ dev-env.el
@@ -1,10 +1,15 @@
 ;; development environment configuration
 (defun c/c++-conf ()
   (hs-minor-mode)
-  (c-set-style "bsd")
+  (c-set-style "linux")
+  (setq c-basic-offset 8)
+  (setq tab-width 8)
+  (setq indent-tabs-mode t)
+  (show-ws-toggle-show-trailing-whitespace)
   )
-
 
 (add-hook 'c-mode-common-hook 'c/c++-conf)
 (add-hook 'c++-mode-hook 'c++-mode-hook)
 (add-hook 'lisp-mode-hook 'hs-minor-mode)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
