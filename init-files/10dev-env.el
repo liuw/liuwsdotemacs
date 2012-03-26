a246f6572c3352bf0be0289d45bba5ea05a8e8c5
Wei Liu <wei.liu2@citrix.com>
2012-03-26 15:32:24 +0100

    Remove vc-mode hook.

--- init-files/10dev-env.el
+++ init-files/10dev-env.el
@@ -1,15 +1,16 @@
 ;; development environment configuration
 (defun c/c++-conf ()
   (hs-minor-mode)
-;;  (c-set-style "linux")
+  (c-set-style "linux")
 ;;  (setq c-basic-offset 8)
 ;;  (setq tab-width 8)
 ;;  (setq indent-tabs-mode t)
-  ;; (show-ws-toggle-show-trailing-whitespace)
-  (c-set-style "bsd")
-  (setq c-basic-offset 4)
-  (setq indent-tabs-mode nil)
-  (setq show-trailing-whitespace t)
+;;  (show-ws-toggle-show-trailing-whitespace)
+;;  (c-set-style "bsd")
+;;  (setq c-basic-offset 4)
+;;  (setq indent-tabs-mode nil)
+;;  (setq show-trailing-whitespace t)
+  (gtags-mode 1)
   )
 
 (add-hook 'c-mode-common-hook 'c/c++-conf)
@@ -17,3 +18,8 @@
 (add-hook 'lisp-mode-hook 'hs-minor-mode)
 
 ;;(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(require 'vc)
+(remove-hook 'find-file-hook 'vc-find-file-hook)
+(require 'xcscope)
+
