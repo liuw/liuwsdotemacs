c34bbd0baa32ec8a0c82fbd706a641c376b1e626
Wei Liu <liuw@liuw.name>
2014-07-23 16:49:02 +0100

    Add some notes in 50haskell.el

--- init-files/50haskell.el
+++ init-files/50haskell.el
@@ -1,1 +1,8 @@
+;; Some notes on Haskell development environment
+;;
+;; C-c C-t: inferior-haskell-type, query type for an expression
+;; C-c C-i: inferior-haskell-info, query info for an expression
+;; C-c C-b: start interpreter
+;;
+
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
