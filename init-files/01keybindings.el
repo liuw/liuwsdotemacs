595da565380b23a2a4cf4829cc4a814f3e8e98bd
Wei Liu <wei.liu2@citrix.com>
2015-03-05 12:56:14 +0000

    Key macro bindings

--- init-files/01keybindings.el
+++ init-files/01keybindings.el
@@ -1,5 +1,10 @@
 ;; Useful key bindings go here
 
 (global-set-key (kbd "C-h a") 'apropos)
+
+(global-set-key [f2] 'kmacro-start-macro-or-insert-counter)
+(global-set-key [f3] 'kmacro-end-or-call-macro)
+(global-set-key [f4] 'call-last-kbd-macro)
+
 (global-set-key [f5] 'point-to-register)
 (global-set-key [f6] 'jump-to-register)
