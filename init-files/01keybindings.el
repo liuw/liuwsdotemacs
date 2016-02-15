e7e7908c8aaea2d4a32851071b7ffe33decf665b
Wei Liu <wei.liu2@citrix.com>
2016-02-15 17:01:12 +0000

    Update key bindings

--- init-files/01keybindings.el
+++ init-files/01keybindings.el
@@ -2,9 +2,13 @@
 
 (global-set-key (kbd "C-h a") 'apropos)
 
-(global-set-key [f2] 'kmacro-start-macro-or-insert-counter)
-(global-set-key [f3] 'kmacro-end-or-call-macro)
-(global-set-key [f4] 'call-last-kbd-macro)
+(global-set-key [f2] 'previous-buffer)
+(global-set-key [f3] 'next-buffer)
+
+
+(global-set-key [f7] 'kmacro-start-macro-or-insert-counter)
+(global-set-key [f8] 'kmacro-end-or-call-macro)
+(global-set-key [f9] 'call-last-kbd-macro)
 
 (global-set-key [f5] 'point-to-register)
 (global-set-key [f6] 'jump-to-register)
