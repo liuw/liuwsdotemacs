e95ab258e6c4afa6d8c550fb76f8d8cc09fab374
Wei Liu <wei.liu2@citrix.com>
2016-02-17 19:54:40 +0000

    Load package repositories unconditionally

--- init-files/00package.el
+++ init-files/00package.el
@@ -1,9 +1,9 @@
-(if (string< emacs-version "24")
-    (progn (require 'package)
-           (add-to-list 'package-archives
-                        '("marmalade" .
-                          "http://marmalade-repo.org/packages/"))
-	   (add-to-list 'package-archives
-			'("melpa" .
-			  "http://melpa.org/packages/"))
-	   (package-initialize)))
+(require 'package)
+
+(add-to-list 'package-archives
+	'("marmalade" .
+	  "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+	'("melpa" .
+	  "http://melpa.org/packages/"))
+(package-initialize)
